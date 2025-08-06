af2c84934a800dab8706c5f88db97562260c86b2
PoHung Huang <pohung.pcs08g@nctu.edu.tw>
2025-08-06 14:34:22 +0800

    Update API script to group by user and hide secret

---
+++ test_codedium_api.sh
@@ -0,0 +1,40 @@
+curl -X POST --header "Content-Type: application/json" \
+--data '{
+  "service_key": "<secret>",
+  "query_requests": [
+    {
+      "data_source": "QUERY_DATA_SOURCE_USER_DATA",
+      "selections": [
+        {
+          "field": "num_acceptances",
+          "name": "total_acceptances",
+          "aggregation_function": "QUERY_AGGREGATION_SUM"
+        },
+        {
+          "field": "num_lines_accepted",
+          "name": "total_lines",
+          "aggregation_function": "QUERY_AGGREGATION_SUM"
+        }
+      ],
+      "filters": [
+        {
+          "name": "hour",
+          "filter": "QUERY_FILTER_GE",
+          "value": "2025-07-15"
+        },
+        {
+          "name": "hour",
+          "filter": "QUERY_FILTER_LE",
+          "value": "2025-08-06"
+        }
+      ],
+      "aggregations": [
+        {
+          "field": "api_key",
+          "name": "user_hash"
+        }
+      ]
+    }
+  ]
+}' \
+https://server.codeium.com/api/v1/Analytics
